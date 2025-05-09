66623b0c6c7d95d623cfe403171995e3f292647d
souhailt0 <94309403+souhailt0@users.noreply.github.com>
2025-05-09 16:52:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector("form");
     form.addEventListener("submit", function (e) {
-        e.preventDefault(); // prevent actual submission unless backend is hooked
         alert("Merci pour votre message ! Nous vous contacterons bientôt.");
         form.reset();
         window.scrollTo({ top: 0, behavior: 'smooth' });
